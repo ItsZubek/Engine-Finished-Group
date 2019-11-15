24c853293266c42fc740572777e0d5fce6de1cb1
Mateusz Subiel <P1720737X@my365.dmu.ac.uk>
2019-11-15 12:23:24 +0000

    Added a test class and implemented the first unit test

---
+++ engineTests/eventTests.cpp
@@ -0,0 +1,10 @@
+#include "eventTests.h"
+
+TEST(Events, ResizeConstructor) {
+	int gotWidth = re.getWidth();
+	int gotHeight = re.getHeight();
+	Engine::EventType type = re.getEventType();
+
+	EXPECT_EQ(gotWidth, width);
+	EXPECT_EQ(gotHeight, height);
+}

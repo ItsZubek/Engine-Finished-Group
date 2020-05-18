cc064c33ec25c2024934c0bdf2dce16f8402a6de
Jack Coulson (Home PC) <P17163701@my365.dmu.ac.uk>
2020-05-18 22:42:19 +0100

    Profiler Class Added

--- engine/enginecode/include/independent/Profiler/profiler.h
+++ engine/enginecode/include/independent/Profiler/profiler.h
@@ -1,22 +1,45 @@
 #pragma once
 
-#include "systems/MyTimer.h"
 #include "imgui.h"
+#include "systems/MyTimer.h"
+#include <iostream>
 
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/type_ptr.hpp> 
 
-namespace Engine {
-
-	class profiler{
-	public:
-		profiler(const char* name);
-		void Stop();
-	private :
+namespace Engine
+{
+	class Profiler
+	{
+	private:
 		const char* m_Name;
-		std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
 		bool m_Stopped;
-	};
-	
+		std::chrono::time_point<std::chrono::steady_clock> m_StartTimePoint;
+	public:
+		Profiler(const char* name)
+			: m_Name(name), m_Stopped(false)
+		{
+			m_StartTimePoint = std::chrono::high_resolution_clock::now();
+		}
 
+		void Stop()
+		{
+			auto endTimePoint = std::chrono::high_resolution_clock::now();
+
+			long long start = std::chrono::time_point_cast<std::chrono::milliseconds>(m_StartTimePoint).time_since_epoch().count();
+			long long end = std::chrono::time_point_cast<std::chrono::milliseconds>(endTimePoint).time_since_epoch().count();
+
+			m_Stopped = true;
+
+			std::cout << "Duration: " << (end - start) << std::endl;
+		};
+		~Profiler()
+		{
+			if (m_Stopped == false)
+			{
+				Stop();
+			}
+		}
+	};
+		
 }
--- engine/enginecode/src/Profiler/Profiler.cpp
+++ engine/enginecode/src/Profiler/Profiler.cpp
@@ -1,15 +1,8 @@
-#include "profiler.h"
 #include "engine_pch.h"
 
-namespace Engine {
-	profiler::profiler(const char * name):m_Name(name),m_Stopped(false){
-		m_StartTimepoint = std::chrono::high_resolution_clock::now();
+#include "Profiler/Profiler.h"
 
-	}
-	void stop() {
-		auto endTimePoint = std::chrono::high_resolution_clock::now();
-		long long start = std::chrono::time_point_cast<std::chrono::milliseconds>(m_StartTimepoint).time_since_epoch().count();
-
-	}
-
+namespace Engine
+{
+	
 }

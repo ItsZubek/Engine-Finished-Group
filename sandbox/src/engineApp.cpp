912bae1b98707ac506bf4d6a84cea2e63fa32311
Jack Coulson (Home PC) <P17163701@my365.dmu.ac.uk>
2020-05-26 17:10:51 +0100

    pushed the layer onto the layer stack in engine app

--- sandbox/src/engineApp.cpp
+++ sandbox/src/engineApp.cpp
@@ -2,9 +2,20 @@
 */
 #include "engineApp.h"
 
+#include "systems/MyTimer.h"
+#include <iostream>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp> 
+#include "GameLayer.h"
+
+#include <chrono>
+
+
 
 engineApp::engineApp()
 {
+	m_layerStack->PushLayer(std::make_shared<GameLayer>(GameLayer("Game Layer")));
 }
 
 engineApp::~engineApp()

e57fcb7671457a0ad72b40211f1d4405cfc808cf
Mateusz Subiel <P1720737X@my365.dmu.ac.uk>
2020-05-27 19:42:45 +0200

    Added audio files

--- engine/enginecode/src/independent/application.cpp
+++ engine/enginecode/src/independent/application.cpp
@@ -83,8 +83,9 @@ namespace Engine {
 
 
 		m_audiosystem.Start();
-		m_audiosystem.LoadSound("assets/audio/movie_1.mp3", false, true);
-		m_audiosystem.PlaySounds("assets/audio/movie_1.mp3", glm::vec3(0, 0, 0), m_audiosystem.VolumeTodB(1.0f));
+		m_audiosystem.LoadSound("assets/audio/sound4.mp3");
+		m_audiosystem.LoadSound("assets/audio/laser.wav");
+		m_audiosystem.PlaySounds("assets/audio/sound4.mp3", glm::vec3(0, 0, 0), m_audiosystem.VolumeTodB(0.02f));
 		
 
 		mp_imgui = std::shared_ptr<Imgui>(ImguiGLFW::initialise());
@@ -213,6 +214,7 @@ namespace Engine {
 		if (e.GetKeyCode() == 68) m_Player->movement(b2Vec2(-0.2f, 0.0f));
 		if (e.GetKeyCode() == 32)
 		{
+			m_audiosystem.PlaySounds("assets/audio/laser.wav", glm::vec3(0, 0, 0), m_audiosystem.VolumeTodB(1.f));
 			b2Vec2 playerPos = m_Player->playerPosition();
 			m_Bullet->setPosition(b2Vec2(playerPos.x, playerPos.y + 0.2));
 			m_Bullet->fire(b2Vec2(0.0f, 0.2f));
